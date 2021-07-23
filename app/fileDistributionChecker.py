7c382fbcaa8a51502e51b44c4e78976059261a78
LittleKross47 <btyoung2k@gmail.com>
2021-07-23 16:54:06 -0400

    -App framework setup in main.py

---
+++ app/fileDistributionChecker.py
@@ -0,0 +1,32 @@
+#Original Source: http://stackoverflow.com/questions/1392413/ddg#1392549
+"""import os
+
+def get_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+
+    return total_size
+
+print(get_size(), 'bytes')"""
+
+import os
+
+
+def get_size_py(start_path='.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+
+    return total_size
+
+
+#print(get_size('C:/Users/btyou/sync'), 'bytes')
---
+++ app/main.py
@@ -0,0 +1,155 @@
+# Imports
+import sys
+import os
+import argparse
+import re
+#import fileinput
+
+# Global variables
+version = "0.0.1"
+stage = "alpha"
+
+# Class
+
+
+class app:
+
+    def __init__(self):
+        folder = 1
+        self.version = version
+        self.stage = stage
+        # Create parser
+        parser = argparse.ArgumentParser(
+            add_help=False, description="Takes a disk and prints its contents.")
+
+        # Add args
+        parser.add_argument("-v", "--version", action="version",
+                            version="File Size Distribution Version --> " + version, help="Display current program version")
+        parser.add_argument("-d", "-dir", "--directory", action="store_true",
+                            help="List byte distribution across folders of the input drive")
+        parser.add_argument("-h", "-H", "--help", "-?", action="help",
+                            default=argparse.SUPPRESS, help="Show this help message and exit.")
+        parser.add_argument("-f", "--file", type=str,
+                            help="Specify a drive file")
+
+# Methods
+
+## Parse the input flags using argparse
+## Void input, returns a argument parser with data
+
+
+def parseInput():
+    # Check if no args were passed
+    if len(sys.argv) == 1 and sys.stdin.isatty():
+        app.parser.print_help(sys.stderr)
+        sys.exit(1)
+    args = app.parser.parse_args()
+
+    #Return parser with args
+    return args
+
+## Takes in an argParser with data and executes main program logic
+
+
+def evalArgs(argsData):
+    try:
+        #print("test")
+        # Predefine vars
+        fileName = None
+        diskFile = None
+        # Check for stdin
+        if not sys.stdin.isatty():
+            diskFile = sys.stdin  # fileinput.input()
+
+        # Check for file input flag
+        if argsData.file != None:
+            fileName = argsData.file
+            diskFile = open(fileName)
+
+        # Check if either stdin or -f has been passed in
+        if diskFile != None:
+            data = collectRawDisk(diskFile)
+            printRawDisk(data)
+
+        # Checks for directory file
+        if argsData.directory or not sys.stdin.isatty():
+            crawlRawDisk(data)
+
+    except:
+        # Check if file does not exist
+        if (fileName != None) and not(verifyFile(fileName)):
+            print(
+                "Error: The disk is broken or does not exist, please provide a correct drive file.")
+        # Check if dir flag was set and no other args
+        elif argsData.directory and fileName == None and diskFile == None and data == []:
+            print("Error: -dir is useless without a specified disk file")
+        else:
+            pass
+        # Catch all other errors
+        # else:
+        #     print(Exception + "Error: you broke the thing... good job")
+
+## Inputs a fileName and checks if that file exists in the CWD
+## Returns true or false
+
+
+def verifyFile(fileName):
+    return os.path.exists(os.path.join(os.getcwd(), fileName))
+
+## Void method inputs a list and prints the contents
+
+
+def printRawDisk(disk):
+    for line in disk:
+        print(line)
+
+## inputs empty list and file, and extracts contents from file to disk
+## Returns filled list
+
+
+def collectRawDisk(file):
+    data = []
+    try:
+        count = 0
+        for line in file:
+            if count > 1:
+                data.append(str(line[3:64]))
+            count += 1
+        return data
+    except:
+        pass
+
+## Void method takes in a list and recursively iterates through all the clusters in the list
+
+
+def crawlRawDisk(*args):
+    if len(args) == 1:
+        currentCluster = args[0][0]
+        startOfVolume = int(currentCluster[5:7], 16)
+        volumeName = currentCluster[7:64]
+        data = readClusterData(volumeName)
+        print("Volume: " + data)
+        crawlRawDisk(args[0], startOfVolume)
+    if len(args) == 2:
+        currentCluster = args[0][args[1]]
+        nextCluster = int(currentCluster[1:3], 16)
+        filename = readClusterData(currentCluster[5:64])
+        print("\tFile: " + filename)
+        if nextCluster != 00:
+            crawlRawDisk(args[0], nextCluster)
+
+
+def hexToStr(hex):
+    hex = re.sub("00.*", "", hex)
+    return bytearray.fromhex(hex).decode()
+
+## Void method takes in a string and evaluates the sector
+
+
+def readClusterData(clusterData):
+    return hexToStr(clusterData)
+
+
+# Main
+parsed = parseInput()
+evalArgs(parsed)
